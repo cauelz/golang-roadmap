85102244607a3474a8b609c1991165a3042e94cd
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-07-16 22:08:21 -0300

    feat: created a simples investiments calculator.

--- src/investiments_calculator/investiments_calculator.go
+++ src/investiments_calculator/investiments_calculator.go
@@ -7,19 +7,26 @@ import (
 
 func main() {
 
-	// How to declare Variables and Constants in Go
-	// var <variable_name> <variable_type> = <value> declare and assign the value to the variable
-	// const <constant_name> <constant_type> = <value> declare and assign the value to the constant
-	// var <variable_name> <variable_type> just declare the variable
-	// <variable_name> = <value> assign the value to the variable
-	// <variable_name> := <value> declare and assign the value to the variable (only works inside functions) and Go will infer the type of the variable
 	const inflationRate = 3.0
-	var investmentAmount float64 = 1000
-	interestRate, years  := 5.5, 5.0
+	//var investmentAmount, interestRate, years float64
+	var (
+		investmentAmount float64
+		interestRate     float64
+		years            float64
+	)
+	
+	fmt.Print("Enter investment amount: ")
+	fmt.Scan(&investmentAmount)
 
-	futureValue := investmentAmount * math.Pow((1 + interestRate/100), years)
+	fmt.Print("Enter interest rate: ")
+	fmt.Scan(&interestRate)
 
-	futureRealValue := futureValue / math.Pow((1 + inflationRate/100), years)
+	fmt.Print("Enter number of years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow((1+interestRate/100), years)
+
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
 	fmt.Print("Future real value is ", futureRealValue)
 }
