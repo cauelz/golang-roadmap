1d572f679554312d43fdea05538ef71a1a9a1d92
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-07-17 19:25:20 -0300

    feat: investments_calculator | created a function called outputText.

--- src/investiments_calculator/investiments_calculator.go
+++ src/investiments_calculator/investiments_calculator.go
@@ -15,13 +15,13 @@ func main() {
 		years            float64
 	)
 	
-	fmt.Print("Enter investment amount: ")
+	outputText("Enter investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Enter interest rate: ")
+	outputText("Enter interest rate: ")
 	fmt.Scan(&interestRate)
 
-	fmt.Print("Enter number of years: ")
+	outputText("Enter number of years: ")
 	fmt.Scan(&years)
 
 	futureValue := investmentAmount * math.Pow((1+interestRate/100), years)
@@ -29,4 +29,8 @@ func main() {
 	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 
 	fmt.Print("Future real value is ", futureRealValue)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
