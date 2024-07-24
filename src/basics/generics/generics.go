3cffb2c183de5e71b592872de93a5bfd3f9cff93
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-07-24 10:29:38 -0300

    feat: generics | created an add functions with generics.

--- src/basics/generics/generics.go
+++ src/basics/generics/generics.go
@@ -13,7 +13,23 @@ func Run() {
 	PrintAnyValue(1.0)
 
 	PrintAnyValue(true)
+
+	PrintAnyValue(add(1, 2))
+
+	PrintAnyValue(add(1.0, 2.0))
+
+	PrintAnyValue(add("Hello", " World"))
 }
+// add is a generic function that accepts any type that is an integer, float64, or string
+// and returns the sum of the two values.
+// A brief explanation of how it works: 
+// The function signature is add[T int | float64 | string](a, b T) T
+// The [T int | float64 | string] part is the type constraint. 
+// It tells the compiler that the function can only accept types that are either int, float64, or string.
+func add[T int | float64 | string](a, b T) T {
+	return a + b
+}
+
 // PrintAnyValue prints the value of any type because it accepts an interface{}
 func PrintAnyValue(value interface{}) {
 	
