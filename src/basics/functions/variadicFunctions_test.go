43dda450b95f10b6fa635dd011aa793b79714681
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-07-28 18:11:17 -0300

    feat: functions | "spreading" values from a slice.

--- src/basics/functions/variadicFunctions_test.go
+++ src/basics/functions/variadicFunctions_test.go
@@ -10,5 +10,11 @@ func TestSumValues(t *testing.T) {
 
 	summedValue := SumValues(1, 2, 3, 4, 5)
 
-	fmt.Println("TestSumValues: ", summedValue)
+	fmt.Println("TestSumValues 1: ", summedValue)
+
+	values := []int{1, 2, 3, 4, 5}
+
+	summedValue = SumValues(1, values...)
+
+	fmt.Println("TestSumValues 2: ", summedValue)
 }
